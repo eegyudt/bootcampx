10e28b7fe82d20d4a9288178ef1d6a6ed91c422c
Eszter Egyud <eszter.egyud@telus.com>
2022-12-12 18:36:28 -0700

    Add query to calculate total assignments/enrolled student if less than 100

---
+++ 2_queries_joins/3_students_total_submissions.sql
@@ -0,0 +1,8 @@
+-- calculate the total number of assignment_submissions for each currently enrolled student individually, whose submissions are less than 100
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING COUNT(assignment_submissions.*) < 100;
