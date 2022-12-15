ed77ed93c2ba0b7ebfa9f0968519f3e06f0f4452
Eszter Egyud <eszter.egyud@telus.com>
2022-12-14 22:52:18 -0700

    Created parameterized query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,39 +8,58 @@ const pool = new Pool({
 });
 
 
-// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests) as total_assistances
-// FROM teachers
-// JOIN assistance_requests ON teacher_id = teachers.id
-// JOIN students ON student_id = students.id
-// JOIN cohorts ON cohort_id = cohorts.id
-// WHERE cohorts.name LIKE '%${process.argv[2]}%'
-// GROUP BY cohorts.name, teachers.name
-// ORDER BY teacher;
 
-// pool.query(`
-// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests) as total_assistances
+
+const cohortsName = process.argv[2];
+
+const val = [`%${cohortsName}%`];
+
+const queryCommand = `
+SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
+FROM teachers
+JOIN assistance_requests
+ON assistance_requests.teacher_id = teachers.id
+JOIN students
+ON students.id = assistance_requests.student_id
+JOIN cohorts
+ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+GROUP BY cohorts.name, teachers.name
+ORDER BY teachers.name;
+
+`;
+
+pool.query(queryCommand, val)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
+
+
+
+
+
+
+// const cohortName = process.argv[2];
+// // Store all potentially malicious values in an array.
+// const value = [`%${cohortName}%`];
+
+// const queryString = `
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 // FROM teachers
 // JOIN assistance_requests ON teacher_id = teachers.id
 // JOIN students ON student_id = students.id
 // JOIN cohorts ON cohort_id = cohorts.id
-// WHERE cohorts.name LIKE '%${process.argv[2]}%'
-// GROUP BY cohorts.name, teachers.name
+// WHERE cohorts.name = $1
 // ORDER BY teacher;
-// `)
-// .then(res => {
-//   res.rows.forEach(console.log)
-// }).catch(err => console.error('query error', err.stack));
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+// `;
+
+
+// pool.query(queryString, value)
+//   .then(res => {
+//     res.rows.forEach(row => {
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     });
+//   })
+//   .catch(err => console.error('query error', err.stack));
